6986adc11f4d4b728e20c716dfc97e56638c6ee8
Casey <caseywheatonwerle@gmail.com>
2023-02-23 08:23:15 -0600

    Add conf_or_req() logic to add more flexibility

--- Make Appointment.py
+++ Make Appointment.py
@@ -1,5 +1,13 @@
 from email_appointment import email_appt_conf,email_appt_request
-from appointment_gui import appointment_info
+from appointment_gui import appointment_info,conf_or_req
+
+
 
 if __name__ == "__main__":
-    email_appt_conf()
+    x = conf_or_req()
+    if x == "Request":
+        email_appt_request(appointment_info())
+    elif x == "Confirm":
+        email_appt_conf()
+    else:
+        raise Exception("Nothing Happened")
